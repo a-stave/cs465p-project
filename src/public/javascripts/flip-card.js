558e283b3ceaa1f1f3353ccf586bd8dac7a6bd60
astave <astave@pdx.edu>
2026-03-18 21:41:47 -0700

    Lots of styling and setup some crude deck_play animations

--- src/public/javascripts/flip-card.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".flip-card").forEach((card) => {
-    card.addEventListener("click", () => {
-      card.classList.toggle("flipped");
-    });
-  });
-});
